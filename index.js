6be54b4e25ee02bacbbcc37f71d63e260d6b1f94
Mike Robards <mikerobards@icloud.com>
2021-05-07 17:35:24 -0400

    add generic bottle img

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 // variable
-const urlBase = "https://api.punkapi.com/v2/beers"
+const urlBase = "https://api.punkapi.com/v2/beers?page="
 const beersDiv = document.querySelector('.beers')
 const filterABV = document.getElementById('filterABV')
-let optionsABV = ''
 const filterIBU = document.getElementById('filterIBU')
+const pageText = document.getElementById('pageNumber')
+const prevPage = document.getElementById('prevPage')
+const nextPage = document.getElementById('nextPage')
+let optionsABV = ''
 let optionsIBU = ''
+let page = 1
+ 
 
 //filters
 
@@ -16,16 +21,16 @@ filterABV.addEventListener("change", e => {
             optionsABV = ''
             break
         case "weak":
-            optionsABV = 'abv_lt=4.6'
+            optionsABV = '&abv_lt=4.6'
             break
         case "medium":
-            optionsABV = 'abv_gt=4.5&abv_lt=7.6'
+            optionsABV = '&abv_gt=4.5&abv_lt=7.6'
             break
         case "strong":
-            optionsABV = 'abv_gt=7.5'
+            optionsABV = '&abv_gt=7.5'
             break
     }
-
+    page = 1
     getBeers()
 })
 
@@ -37,35 +42,53 @@ filterIBU.addEventListener("change", e => {
             optionsIBU = ''
             break
         case "weak":
-            optionsIBU = 'abv_lt=35'
+            optionsIBU = '&ibu_lt=35'
             break
         case "medium":
-            optionsIBU = 'ibu_gt=34&ibu_lt=75'
+            optionsIBU = '&ibu_gt=34&ibu_lt=75'
             break
         case "strong":
-            optionsIBU = 'abv_gt=74'
+            optionsIBU = '&ibu_gt=74'
             break
     }
 
+    page = 1
     getBeers()
 })
 
 
 async function getBeers() {
-    const url = urlBase + "?" + optionsABV + "&" + optionsIBU
+    const url = urlBase + page + optionsABV + optionsIBU
     console.log(url)
     const beerPromise = await fetch(url)
     const beers = await beerPromise.json()
 
     console.log(beers[0])
 
+    // pagination
+    pageText.innerHTML = page
+
+    if(page === 1){
+        prevPage.disabled = true
+    } else {
+        prevPage.disabled = false
+    }
+
+    if(beers.length < 25) {
+        nextPage.disabled = true 
+    } else {
+        nextPage.disabled = false
+    }
+
     let beersHTML = ''
+
+    const genericBottle = 'https://cdn.pixabay.com/photo/2014/12/22/00/04/bottle-576717_960_720.png'
 
     beers.forEach(beer => {
         beersHTML += `
             <div class="beer-wrapper card">
                 <div class="beer">
-                    <img class="beer__img" src="${beer.image_url}">
+                    <img class="beer__img" src="${beer.image_url ? beer.image_url : genericBottle}">
                     <h3>${beer.name}</h3>
                     <span class="beer__info">
                         <span>ABV: ${beer.abv}%</span>
@@ -89,5 +112,18 @@ async function getBeers() {
     beersDiv.innerHTML = beersHTML
 
 }
+
+//pagination 
+
+prevPage.addEventListener("click", () =>{
+    page--
+    getBeers()
+})
+
+
+nextPage.addEventListener("click", () =>{
+    page++
+    getBeers()
+})
 
 getBeers()
